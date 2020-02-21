e3ba8d0d3091c644fc8502369cec05986e4dc338
ikarosf <3316741951@qq.com>
2020-02-21 15:35:51 +0800

    增加数值合法性检测

--- battleready.py
+++ battleready.py
@@ -28,71 +28,80 @@ class Ui_battleReadyForm(object):
         self.gridLayout.setObjectName(u"gridLayout")
         self.mycardlistWidget = QListWidget(Form)
         self.mycardlistWidget.setObjectName(u"mycardlistWidget")
+        Form.setStyleSheet("""
+    QListView::item:selected { color: blue; }
+    QListView::item:selected { background-color: white; }
+    QListView::item:selected { border: 1px solid red; }
+        """)
 
         self.gridLayout.addWidget(self.mycardlistWidget, 0, 0, 1, 2)
 
         self.enemycardlistView = QListWidget(Form)
         self.enemycardlistView.setObjectName(u"enemycardlistView")
-
+        self.enemycardlistView.setSelectionMode(QAbstractItemView.MultiSelection)
         self.gridLayout.addWidget(self.enemycardlistView, 0, 2, 1, 2)
 
         self.npclistView = QListWidget(Form)
         self.npclistView.setObjectName(u"npclistView")
-
+        self.npclistView.setSelectionMode(QAbstractItemView.MultiSelection)
         self.gridLayout.addWidget(self.npclistView, 0, 4, 1, 2)
 
-        self.mycardpushButton = QPushButton(Form)
-        self.mycardpushButton.clicked.connect(self.chooseMyCard)
-        self.mycardpushButton.setObjectName(u"mycardpushButton")
-        self.gridLayout.addWidget(self.mycardpushButton, 1, 0, 1, 1)
+        # self.mycardpushButton = QPushButton(Form)
+        # self.mycardpushButton.clicked.connect(self.chooseMyCard)
+        # self.mycardpushButton.setObjectName(u"mycardpushButton")
+        # self.gridLayout.addWidget(self.mycardpushButton, 1, 0, 1, 1)
+        self.flashButton = QPushButton(Form)
+        self.flashButton.clicked.connect(self.flash)
+        self.flashButton.setObjectName(u"mycardpushButton")
+        self.gridLayout.addWidget(self.flashButton, 1, 0, 1, 1)
+        #
+        # self.addenemycardpushButton = QPushButton(Form)
+        # self.addenemycardpushButton.setObjectName(u"addenemycardpushButton")
+        # self.addenemycardpushButton.clicked.connect(self.chooseEnemyCard)
+        # self.gridLayout.addWidget(self.addenemycardpushButton, 1, 1, 1, 1)
+        #
+        # self.delenemycardpushButton = QPushButton(Form)
+        # self.delenemycardpushButton.setObjectName(u"delenemycardpushButton")
+        # self.delenemycardpushButton.clicked.connect(self.delEnemyCard)
+        # self.gridLayout.addWidget(self.delenemycardpushButton, 1, 2, 1, 1)
+        #
+        # self.delnpcpushButton = QPushButton(Form)
+        # self.delnpcpushButton.setObjectName(u"delnpcpushButton")
+        # self.delnpcpushButton.clicked.connect(self.delNpc)
+        # self.gridLayout.addWidget(self.delnpcpushButton, 1, 4, 1, 1)
+        #
+        # self.addnpcpushButton = QPushButton(Form)
+        # self.addnpcpushButton.setObjectName(u"addnpcpushButton")
+        # self.addnpcpushButton.clicked.connect(self.chooseNpcCard)
+        # self.gridLayout.addWidget(self.addnpcpushButton, 1, 3, 1, 1)
 
-        self.addenemycardpushButton = QPushButton(Form)
-        self.addenemycardpushButton.setObjectName(u"addenemycardpushButton")
-        self.addenemycardpushButton.clicked.connect(self.chooseEnemyCard)
-        self.gridLayout.addWidget(self.addenemycardpushButton, 1, 1, 1, 1)
-
-        self.delenemycardpushButton = QPushButton(Form)
-        self.delenemycardpushButton.setObjectName(u"delenemycardpushButton")
-        self.delenemycardpushButton.clicked.connect(self.delEnemyCard)
-        self.gridLayout.addWidget(self.delenemycardpushButton, 1, 2, 1, 1)
-
-        self.delnpcpushButton = QPushButton(Form)
-        self.delnpcpushButton.setObjectName(u"delnpcpushButton")
-        self.delnpcpushButton.clicked.connect(self.delNpc)
-        self.gridLayout.addWidget(self.delnpcpushButton, 1, 4, 1, 1)
-
-        self.addnpcpushButton = QPushButton(Form)
-        self.addnpcpushButton.setObjectName(u"addnpcpushButton")
-        self.addnpcpushButton.clicked.connect(self.chooseNpcCard)
-        self.gridLayout.addWidget(self.addnpcpushButton, 1, 3, 1, 1)
 
         self.battlepushButton = QPushButton(Form)
         self.battlepushButton.setObjectName(u"battlepushButton")
-        self.battlepushButton.clicked.connect(self.runTest)
+        self.battlepushButton.clicked.connect(self.runTest2)
         self.gridLayout.addWidget(self.battlepushButton, 1, 5, 1, 1)
 
         self.retranslateUi(Form)
 
         QMetaObject.connectSlotsByName(Form)
-
+        self.flash()
     # setupUi
 
     def retranslateUi(self, Form):
         Form.setWindowTitle("战斗准备")
-        self.mycardpushButton.setText("选择我的卡片")
-        self.addenemycardpushButton.setText("添加敌方卡片")
-        self.delenemycardpushButton.setText("移除选择敌方")
-        self.delnpcpushButton.setText("移除选择NPC")
-        self.addnpcpushButton.setText("添加NPC")
+        # self.mycardpushButton.setText("选择我的卡片")
+        self.flashButton.setText("刷新选项")
+        # self.addenemycardpushButton.setText("添加敌方卡片")
+        # self.delenemycardpushButton.setText("移除选择敌方")
+        # self.delnpcpushButton.setText("移除选择NPC")
+        # self.addnpcpushButton.setText("添加NPC")
         self.battlepushButton.setText("模拟战斗！")
 
     # retranslateUi
 
     def open(self):
         global_env.mainWin.hide()
-        # self.mycardlistWidget.clear()
-        # self.enemycardlistView.clear()
-        # self.npclistView.clear()
+        # self.flash()
         self.show()
 
     def closeEvent(self, event):
@@ -157,6 +166,29 @@ class Ui_battleReadyForm(object):
         global_env.mainWin.textBrowser.setText(thisText)
         self.close()
 
+    def runTest2(self):
+        if len(self.mycardlistWidget.selectedItems()) == 0:
+            QMessageBox.critical(self, "错误", "未选择我方卡片", QMessageBox.Yes)
+            return
+        if len(self.enemycardlistView.selectedItems()) == 0 and len(self.npclistView.selectedItems()) == 0:
+            QMessageBox.critical(self, "错误", "未选择敌人", QMessageBox.Yes)
+            return
+        file_path = os.path.join(".", "newkf.in")
+        # file_path = os.path.join("E:\\tools\\newkf", "newkf.in")
+        gu_text = self.make_full_gu_text2()
+        if not gu_text:
+            QMessageBox.critical(self, "错误", "选择的数据可能已被删除", QMessageBox.Yes)
+            return
+        with open(file_path, "w") as f:
+            f.write(gu_text)
+        result = execCmd()
+        thisText = ""
+        for i in result:
+            thisText += i
+
+        global_env.mainWin.textBrowser.setText(thisText)
+        self.close()
+
     def make_full_gu_text(self):
         try:
             myCardName = self.mycardlistWidget.item(0).text()
@@ -193,3 +225,54 @@ class Ui_battleReadyForm(object):
         text += "GEAR\nENDGEAR"
 
         return text
+
+    def make_full_gu_text2(self):
+        try:
+            myCardName = self.mycardlistWidget.selectedItems()[0].text()
+            myCard = global_env.myCardList[myCardName]
+
+            enemyCardList = []
+            npcList = []
+            for i in self.enemycardlistView.selectedItems():
+                enemyCardName = i.text()
+                enemyCardList.append(global_env.enemyCardList[enemyCardName])
+
+            for i in self.npclistView.selectedItems():
+                npcName = i.text()
+                npcList.append(global_env.npcList[npcName])
+        except KeyError:
+            return False
+
+        text = ""
+        text += myCard.make_gu_text()
+        text += "\n"
+        text += "NPC\n"
+        for i in npcList:
+            text += i.make_gu_text()
+            text += "\n"
+        text += "ENDNPC\n"
+        text += "\n"
+
+        text += "PC\n"
+        for i in enemyCardList:
+            text += i.make_gu_text()
+            text += '\n'
+        text += "ENDPC\n"
+
+        text += "GEAR\nENDGEAR"
+
+        return text
+
+    def flash(self):
+        self.mycardlistWidget.clear()
+        self.enemycardlistView.clear()
+        self.npclistView.clear()
+        for i in global_env.myCardList.keys():
+            if i != "新卡片":
+                self.mycardlistWidget.addItem(i)
+        for i in global_env.enemyCardList.keys():
+            if i != "新卡片":
+                self.enemycardlistView.addItem(i)
+        for i in global_env.npcList.keys():
+            if i != "新npc":
+                self.npclistView.addItem(i)
